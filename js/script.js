e5753e1769887634c5cf747ae916b075d60fa64b
synnestorm <synne.storm@gmail.com>
2025-05-20 13:39:52 +0200

    add variables

--- js/script.js
+++ js/script.js
@@ -2,16 +2,27 @@
 
 // Written logic to get the computer choice
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 3);
+  let computerChoice = Math.floor(Math.random() * 3) + 1;
   if (computerChoice === 1) {
     console.log("Rock");
     return;
   } else if (computerChoice === 2) {
     console.log("Paper");
     return;
+  } else if (computerChoice === 3) {
+    console.log("Scissor");
+    return;
   }
 }
 
-console.log(getComputerChoice());
+// Written logic to get the human choice
+function getHumanChoice() {
+  let humanChoice = prompt("Choose: Rock, Paper or Scissors", "");
+  console.log(`You chose: ${humanChoice}`);
+  return;
+}
 
-//
+// Variables
+
+humanScore = 0;
+computerScore = 0;
