dfc3fdea26ad14c8b0770b1936bd2c86da6db165
synnestorm <synne.storm@gmail.com>
2025-05-20 13:02:21 +0200

    adding function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-// function
+// Let's play rock, paper, scissors in the console
+
+// Written logic to get the computer choice
 function getComputerChoice() {
-  return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * 3);
 }
+
+console.log(getComputerChoice());
+
+//
